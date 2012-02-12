cac2ac76ed9d27a88392c718768e38193d77e9c7
David Hoerl <dhoerl@mac.com>
2012-02-12 17:27:39 -0500

    STUFF

--- PhotoScrollerNetwork/main.m
+++ PhotoScrollerNetwork/main.m
@@ -1,12 +1,3 @@
-//
-//  main.m
-//  PhotoScrollerNetwork
-//
-//  Created by David Hoerl on 2/3/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
-#import <UIKit/UIKit.h>
 
 #import "AppDelegate.h"
 
